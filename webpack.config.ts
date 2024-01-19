7ccab754f5f9fc2528f17eedf90563f28bb37481
Gusenov Marat <gusenovmaratr@gmail.com>
2024-01-19 21:09:25 +0300

    DevServer config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,22 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
-export default (env: any) => {
+type Mode = 'production' | 'development';
+
+interface EnvVariables {
+  mode: Mode;
+  port: number;
+}
+
+export default (env: EnvVariables) => {
+
+  const isDev = env.mode === 'development';
+
   const config: webpack.Configuration = {
     mode: env.mode ?? 'development',
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
     output: {
       path: path.resolve(__dirname, 'build'),
       filename: '[name].[contenthash].js',
@@ -17,6 +28,8 @@ export default (env: any) => {
     module: {
       rules: [
         {
+          // ts-loader
+          // если бы мы не использовали typescript: нужен был бы babael-loader
           test: /\.tsx?$/,
           use: 'ts-loader',
           exclude: /node_modules/,
@@ -26,6 +39,11 @@ export default (env: any) => {
     resolve: {
       extensions: ['.tsx', '.ts', '.js'],
     },
+    devtool: isDev ? 'inline-source-map' : false,
+    devServer: isDev ? {
+      port: env.port ?? 3000,
+      open: true,
+    } : undefined,
   }
   return config;
 }
