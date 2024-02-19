14534daf3013b5bec95c1f14a601372a083e8fc5
Gusenov Marat <gusenovmaratr@gmail.com>
2024-02-19 23:24:25 +0300

    add fonts webpack

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -8,83 +8,89 @@ import { buildBabelLoader } from "./babel/buildBabelLoader";
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
   const isDev = options.mode === 'development';
 
-	const svgLoader = {
-		test: /\.svg$/i,
-		issuer: /\.[jt]sx?$/,
-		use: [
-			{ 
-				loader: '@svgr/webpack',
-				options: {
-					icon: true,
-					svgConfig: {
-						plugins: [
-							{
-								name: "convertColors",
-								params: {
-									currentColors: 'true',
-								}
-							}
-						]
-					}
-				} 
-			}
-		],
-	}
+  const svgLoader = {
+    test: /\.svg$/i,
+    issuer: /\.[jt]sx?$/,
+    use: [
+      {
+        loader: '@svgr/webpack',
+        options: {
+          icon: true,
+          svgConfig: {
+            plugins: [
+              {
+                name: "convertColors",
+                params: {
+                  currentColors: 'true',
+                }
+              }
+            ]
+          }
+        }
+      }
+    ],
+  }
 
-	const assetLoader = {
-		test: /\.(png|jpg|jpeg|gif)$/i,
-		type: 'asset/resource',
-	}
+  const imageLoader = {
+    test: /\.(png|jpg|jpeg|gif)$/i,
+    type: 'asset/resource',
+  }
 
-	const CssLoaderWithModules = {
-		loader: "css-loader",
-		options: {
-			modules: {
-				localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]'
-			}
-		},
-	}
+  const fontLoader = {
+    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+    type: 'asset/resource',
+  }
+
+  const CssLoaderWithModules = {
+    loader: "css-loader",
+    options: {
+      modules: {
+        localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]'
+      }
+    },
+  }
 
   const scssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
       // Creates 'style' nodes from JS strings
-			isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
       // Translates CSS into CommonJS
-			CssLoaderWithModules,
-			// Complites Sass to CSS
-			"sass-loader",
+      CssLoaderWithModules,
+      // Complites Sass to CSS
+      "sass-loader",
     ],
   }
 
-	const tsLoader = {
-		// ts-loader
-		// если бы мы не использовали typescript: нужен был бы babael-loader
+  const tsLoader = {
+    // ts-loader
+    // если бы мы не использовали typescript: нужен был бы babael-loader
     exclude: /node_modules/,
-		test: /\.tsx?$/,
-		use: [
-			{
-				loader: 'ts-loader',
-				options: {
-						transpileOnly: true,
-						getCustomTransformers: () => (
-						{
-							before: [isDev && ReactRefreshTupescript()].filter(Boolean),
-						}
-					)
-				}
-			}
-		]
+    test: /\.tsx?$/,
+    use: [
+      {
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+          getCustomTransformers: () => (
+            {
+              before: [isDev && ReactRefreshTupescript()].filter(Boolean),
+            }
+          )
+        }
+      }
+    ]
   }
 
-	const babelLoader = buildBabelLoader(options);
+  const babelLoader = buildBabelLoader(options);
 
 
   return [
-		assetLoader,
+    imageLoader,
+    fontLoader,
     scssLoader,
     tsLoader,
-		babelLoader,
-		svgLoader,
+    babelLoader,
+    svgLoader,
   ]
 }
